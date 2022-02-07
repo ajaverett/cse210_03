7e6c0273eff3ef469cc3cbfe0c1199b0aa81473f
TicTacToe333 <aidencedwards@gmail.com>
2022-02-07 09:25:58 -0700

    Mostly made jumper work

---
+++ jumper.py
@@ -0,0 +1,10 @@
+class Jumper:
+    def Jumper(self):
+        self._parachute_list = []
+
+        def display_parachute(self, lives):
+            if lives == 0:
+                return False
+                
+            for i in lives:
+                print (self._parachute_list[i])
