f3b11161da241cf924e5c676452d5f6a9a523ed6
TicTacToe333 <aidencedwards@gmail.com>
2022-02-07 10:01:03 -0700

    Added comments

--- jumper.py
+++ jumper.py
@@ -6,8 +6,10 @@ class Jumper:
         if lives == 0:
             return False
 
+        #removes elements that are no longer present because of incorrect guesses
         for i in range(0, (8- lives)):
             self._parachute_list.remove(self._parachute_list[0])
-            
+
+        #prints the parachute list to the screen, displaying how many lives the user still has
         for i in range(0,lives):
            print (self._parachute_list[i])       
