1d07a6577f0586264836bf1319ae0b086658c9b2
TicTacToe333 <aidencedwards@gmail.com>
2022-02-07 09:58:08 -0700

    Added the list for the jumper man, as well as a way to print him

--- jumper.py
+++ jumper.py
@@ -1,10 +1,13 @@
 class Jumper:
-    def Jumper(self):
-        self._parachute_list = []
+    def __init__(self):
+        self._parachute_list = ['    _______ ' , '   /       \ ', '  / \ / \ / \ ', '  \  | _ |  / ', '  o( ͡° ͜ʖ ͡°)o ', '   \/ |_|\/ ', "     // \\  ", '     U   U ']
 
-        def display_parachute(self, lives):
-            if lives == 0:
-                return False
-                
-            for i in lives:
-                print (self._parachute_list[i])
+    def display_parachute(self, lives):
+        if lives == 0:
+            return False
+
+        for i in range(0, (8- lives)):
+            self._parachute_list.remove(self._parachute_list[0])
+            
+        for i in range(0,lives):
+           print (self._parachute_list[i])       
